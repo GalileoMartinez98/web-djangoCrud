fb288f6c883ae8c0b72c971c32e0925cfc81fdec
Galileo Martínez <52017405+GalileoMartinez98@users.noreply.github.com>
2022-09-30 02:37:21 -0500

    Update settings.py

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Paith
 from pathlib import Path
+
 import os
 import dj_database_url
 
